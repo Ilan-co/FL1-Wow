21a6731f71e3f954ba1ba8a11873d39c22c42178
Eric Chheu <eric.chheu@epitech.eu>
2020-11-08 17:55:47 +0100

    Fix Feed List

--- flutagram/lib/Pages/feed/feed_tile.dart
+++ flutagram/lib/Pages/feed/feed_tile.dart
@@ -3,43 +3,42 @@ import 'package:flutter/material.dart';
 
 class PictureTile extends StatelessWidget {
   const PictureTile({this.feedPicture});
+
   final Map<String, String> feedPicture;
 
   Widget _displayFeed() {
     final List<Widget> list = <Widget>[];
     if (feedPicture != null) {
-      for (int i = 0; i < feedPicture.length; i++) {
-        list.add(
-          Card(
-            margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
-            child: Column(
-              children: <Widget>[
-                const SizedBox(
-                  height: 15,
-                ),
-                Image(
-                  image: NetworkImage(feedPicture['image']),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Text(
-                  feedPicture['location'],
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-              ],
-            ),
+      list.add(
+        Card(
+          margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 15,
+              ),
+              Image(
+                image: NetworkImage(feedPicture['image']),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Text(
+                feedPicture['location'],
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+            ],
           ),
-        );
-        list.add(
-          const SizedBox(
-            height: 15,
-          ),
-        );
-      }
+        ),
+      );
+      list.add(
+        const SizedBox(
+          height: 15,
+        ),
+      );
     }
     return Column(children: list);
   }
